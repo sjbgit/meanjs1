5e17adbb7af817772ff9a3c9f3cdcc8c5cd9ae31
sjbgit <sjb_strat@hotmail.com>
2015-07-13 16:32:49 -0500

    db

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://test:test@ds049211.mongolab.com:49211/mean-dev',
+	db: 'mongodb://mean-dev:mean-dev@ds049211.mongolab.com:49211/mean-dev',
 	app: {
 		title: 'MEAN - Development Environment'
 	},
